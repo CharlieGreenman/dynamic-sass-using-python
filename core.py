ecab1069a10ec8dcc79adf0be3a1d2ec776b0f7d
CharlieGreenman <charlie@Charlies-MacBook-Pro.local>
2015-07-01 20:40:03 -0400

    add more values

--- core.py
+++ core.py
@@ -1,5 +1,9 @@
 __author__ = 'charlie'
 
-for x in range(0, 16):
+for x in range(0, 17):
     print '$L' + str(x) + ': $num * ' + str(x) + 'px;'
+
+for x in range(0, 17):
+    print '$X' + str(x) + ': $num * ' + str(x) + 'px;'
+
 
